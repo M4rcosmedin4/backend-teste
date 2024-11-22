16661c537a40b4f2d33d7ebc52c7b21121eeb4c6
M4rcosmedin4 <124940350+M4rcosmedin4@users.noreply.github.com>
2024-11-22 18:39:59 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const prom = require('prom-client');
 const itemRoutes = require('./routes/itemRoutes');
-const register = prom.register
 
 
 const app = express();
@@ -28,26 +26,11 @@ mongoose
   .catch((err) => console.error('Erro ao conectar ao MongoDB:', err));
 
 
-app.get('/metrics',async function(req,res) {
-  res.set('Content-type',register.contentType)
-  res.end(await register.metrics())
-})
 
 app.get('/',(req,res)=>{
-  counter.inc()
   res.send('hello world')
 })
 
-const frontendAccessCounter = new prom.Counter({
-  name: 'frontend_access_total',
-  help: 'Contador de acessos ao frontend',
-});
-
-// Rota para rastrear acessos ao frontend
-app.post('/track-frontend-access', (req, res) => {
-  frontendAccessCounter.inc(); // Incrementa o contador
-  res.status(200).send('Acesso rastreado com sucesso');
-});
 
 
 

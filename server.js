e0f261a817875ec89e4a7130369f46a100dce8e2
M4rcosmedin4 <124940350+M4rcosmedin4@users.noreply.github.com>
2024-11-22 18:34:24 -0300

    Update server.js

--- server.js
+++ server.js
@@ -55,6 +55,6 @@ app.post('/track-frontend-access', (req, res) => {
 app.use('/api/items', itemRoutes);
 
 // Iniciar o servidor
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Servidor rodando na porta ${port}`);
 });
